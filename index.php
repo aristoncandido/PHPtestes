ef5d60049b6d4e615ce7e3e6c235ed45dbdb81fe
ariston candido <aristoncandido.nw@gmail.com>
2023-06-04 09:19:58 -0300

    alguns testes em PHP

---
+++ index.php
@@ -0,0 +1,54 @@
+
+<?php
+      
+      function TesteFunc(){
+
+        return "<center>minha primeira função em PHP </center>";
+
+      }
+
+
+
+      function Soma($x,$y){
+
+
+            $s = $x+$y;
+
+            return $s;
+
+      }
+
+
+        $f = '<h1>Olá mundo, meu primeiro código em PHP HEHE oi</h1>';
+        echo $f ;
+        echo '<br/>';
+        echo $f;
+        echo $f;
+
+        
+
+
+        $f = '<h2>usei a mesma var para mudar o seu conteúdo </h2>';
+
+        echo $f;
+
+
+
+       echo TesteFunc();
+
+
+
+
+          
+       echo Soma(3,5);
+
+
+
+
+
+
+
+
+
+
+?> 
